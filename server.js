f6d334b9ff4f51c3707fcb3f0675ed75770a6afb
Chong Patterson <chong@thinkful.com>
2018-06-06 12:22:30 -0700

    created POST endpoint for recipes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList, Recipes} = require('./models');
+const { ShoppingList, Recipes } = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -31,10 +31,14 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
+
 app.post('/shopping-list', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
-  for (let i=0; i<requiredFields.length; i++) {
+  for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
       const message = `Missing \`${field}\` in request body`
@@ -47,10 +51,21 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.post('/recipes', jsonParser, (req, res) => {
+  const recipeFields = ['name', 'ingredients'];
+  for (let i = 0; i < recipeFields.length; i++) {
+    const field = recipeFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
 
-app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
-})
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
